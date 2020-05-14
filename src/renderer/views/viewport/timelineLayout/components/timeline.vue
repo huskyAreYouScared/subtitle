d4a88bd32d5a220ecf9fd672a31ded1cac8db351
huskyareyouscared <1710632585@qq.com>
2020-05-14 22:39:13 +0800

    feat(track timeline): add track components
    new add track,build mouse slider event

--- src/renderer/views/viewport/timelineLayout/components/timeline.vue
+++ src/renderer/views/viewport/timelineLayout/components/timeline.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="tl-timeline-container">
-    <section class="timeline-tool-bar">
-
-    </section>
     <section class="subtitle-ctrl-container"> 
-
+      
     </section>
+    <subtitlesTrack/>
   </div>
 </template>
 
 <script>
+import subtitlesTrack from './track'
 export default {
-
+  components: {
+    subtitlesTrack
+  }
 }
 </script>
 
---
+++ src/renderer/views/viewport/timelineLayout/components/track.vue
@@ -0,0 +1,46 @@
+<template>
+  <section
+    ref="track" 
+    class="subtitle-tract-container"
+    @mousedown="mouseDown"
+    @mouseup="mouseUp"
+    @mousemove="mouseMove"
+    @mouseleave="mouseLeave"
+    >
+    <div class="scale-second" v-for="item in 100" :key="item"></div>
+  </section>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      isMove: false,
+      lastXPostion: 0
+    }
+  },
+  methods: {
+    mouseDown (e) {
+      this.isMove = true
+      this.lastXPostion = e.clientX
+    },
+    mouseUp (e) {
+      this.isMove = false
+    },
+    mouseLeave (e) {
+      this.isMove = false
+    },
+    mouseMove (e) {
+      if (this.isMove) {
+        let moveDistance = this.lastXPostion - e.clientX
+        this.$refs.track.scrollLeft += moveDistance
+        this.lastXPostion = e.clientX
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
